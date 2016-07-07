15ca207aebaa4101292e4b7f28c049ebe5c6cc1b
zkzofn <zkzofn@gmail.com>
2016-07-07 22:17:30 +0900

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -5,46 +5,49 @@ import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+
 const API_KEY = 'AIzaSyDLQ4sm-k2OKzxcBAXI7OeJmeZLedr9bMo';
 
 
 // Create a new component. This component should produce come HTML
 class App extends Component {
-    constructor (props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            videos: [],
-            selectedVideo:null
-        };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-        this.videoSearch('surfboards')
-    }
+    this.videoSearch('surfboards')
+  }
 
-    videoSearch(term) {
-        YTSearch({key: API_KEY, term: term}, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
 
-    render () {
-        // _.debounce from lodash 를 사용하는 이유는 약간의 검색할 때 약간의 delay 를 주기 위해서이다. (300ms)
-        // like google search
-        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+  render() {
+    // _.debounce from lodash 를 사용하는 이유는 약간의 검색할 때 약간의 delay 를 주기 위해서이다. (300ms)
+    // like google search
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term)
+    }, 300);
 
-        return (
-            <div>
-                <SearchBar onSearchTermChange={videoSearch} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-                    videos={this.state.videos} />
-            </div>
-        );
-    }
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos}/>
+      </div>
+    );
+  }
 
 };
 
